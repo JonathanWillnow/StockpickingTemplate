3bb4e1c1a17cc4becac26426e75781cb087cf129
JonathanWillnow <82315811+JonathanWillnow@users.noreply.github.com>
2021-05-21 12:12:21 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+
+# Function chunks sourced from 
+# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+     
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+        
+
+        
+        
+def portfolio_input():
+    global portfolio_size
+    portfolio_size = input("Enter the value of your portfolio:")
+
+    try:
+        val = float(portfolio_size)
+    except ValueError:
+        print("That's not a number! \n Try again:")
+        portfolio_size = input("Enter the value of your portfolio:"
