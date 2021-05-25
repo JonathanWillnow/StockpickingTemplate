5ae0dc8f21300e938be11f6a93f03f944395e132
JonathanWillnow <82315811+JonathanWillnow@users.noreply.github.com>
2021-05-25 22:49:34 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,10 +6,7 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-        
-
-        
-        
+             
 def portfolio_input():
     global portfolio_size
     portfolio_size = input("Enter the value of your portfolio:")
@@ -18,4 +15,4 @@ def portfolio_input():
         val = float(portfolio_size)
     except ValueError:
         print("That's not a number! \n Try again:")
-        portfolio_size = input("Enter the value of your portfolio:"
+        portfolio_size = input("Enter the value of your portfolio:")
